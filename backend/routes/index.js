ada5b17d0928f37b26cf6aa49f3624c141db625f
Somesh Chavhan <someshchavhan904@gmail.com>
2023-06-28 13:14:25 +0530

    all the property releated routes added

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,14 +1,24 @@
 import express from 'express'
 import { loginUser, registerUser } from '../controllers/user.js'
+import { verifyToken } from "../middlewares/verifyToken.js"
+import { getAll, getFetured, getSpecific, getType, getTypeCount, createProperty, updateProperty, deleteProperty } from '../controllers/property.js'
 
 const router = express.Router()
 
 
-router.get('/', (req, res) => {
-    res.send('hello');
-})
 
 router.post('/register', registerUser)
 router.post('/login', loginUser)
+
+
+router.get('/getAll', getAll);
+router.get('/findFetured', getFetured);
+router.get('/find', getType);
+router.get('/find/type', getTypeCount);
+router.get('/find/:id', getSpecific);
+
+router.post('/', verifyToken, createProperty);
+router.put('/:id', verifyToken, updateProperty);
+router.delete('/:id', verifyToken, deleteProperty)
 
 export default router
